44b4dbf078195f6ba30ad584ded744523aaa4b94
“Cristian <cristiancanapi730@gmail.com>
2023-08-28 15:22:47 -0400

    Seleccionar y mostrar estado

--- src/presenter.js
+++ src/presenter.js
@@ -5,6 +5,7 @@ const segundo = document.querySelector("#precio-item");
 const form = document.querySelector("#sumar-form");
 const div = document.querySelector("#resultado-div");
 const div2 = document.querySelector("#precio-div")
+const estado = document.querySelector("#est");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -13,6 +14,6 @@ form.addEventListener("submit", (event) => {
   const precioItem = Number.parseInt(segundo.value);
 
   div.innerHTML = "<p> Cantidad de item: " + firstNumber + "</p>";
-  div2.innerHTML = "<p> Precio por item: " + precioItem + "</p>";
+  div2.innerHTML = "<p> Precio por item: " + precioItem +"</p>"+"Estado: " + estado.value;
 
 });
