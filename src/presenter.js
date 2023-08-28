8c10ac8e6803d6043567a5d4bd7cf9d6066766e6
“Cristian <cristiancanapi730@gmail.com>
2023-08-28 15:10:27 -0400

    Ingresas y mostrar precio por item

--- src/presenter.js
+++ src/presenter.js
@@ -1,13 +1,18 @@
 import sumar from "./sumador";
 
 const first = document.querySelector("#primer-numero");
+const segundo = document.querySelector("#precio-item");
 const form = document.querySelector("#sumar-form");
 const div = document.querySelector("#resultado-div");
+const div2 = document.querySelector("#precio-div")
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const firstNumber = Number.parseInt(first.value);
+  const precioItem = Number.parseInt(segundo.value);
 
-  div.innerHTML = "<p>" + firstNumber + "</p>";
+  div.innerHTML = "<p> Cantidad de item: " + firstNumber + "</p>";
+  div2.innerHTML = "<p> Precio por item: " + precioItem + "</p>";
+
 });
